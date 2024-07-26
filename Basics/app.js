b850e1e3e2afb6885bd78bed4e09256d32bce868
wilderif <woojuro3@gmail.com>
2024-07-26 17:48:35 +0900

    feat(Basics): Separate routing logic into a separate file
    - Separated the routing logic into a dedicated file.
    - Used  to export the requestHandler function.
    - Updated the server creation to use the exported requestHandler function with createServer.

--- Basics/app.js
+++ Basics/app.js
@@ -1,59 +1,7 @@
 const http = require("http");
-const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  // console.log(req.url);
-  // console.log(req.method);
-  // console.log(req.headers);
+const routes = require("./routes.js");
 
-  const url = req.url;
-  const method = req.method;
-
-  console.log(`New request: ${req.method} ${req.url}`);
-  if (url === "/") {
-    // console.log("in 0!!!");
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
-    res.write(
-      "<body><form action='/message' method='POST'><input type='text' name='message'><button tyoe='submit'>Send</button></input></form></body>"
-    );
-    res.write("</html>");
-    return res.end();
-  }
-
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      // console.log(chunk);
-      body.push(chunk);
-    });
-
-    // console.log("in 1!!!");
-
-    return req.on("end", () => {
-      // console.log("in 2!!!");
-      const parsedBody = Buffer.concat(body).toString();
-      // console.log(parsedBody);
-      const message = parsedBody.split("=")[1];
-      // console.log(message);
-
-      fs.writeFile("message.txt", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-      // console.log("end!!!");
-    });
-  }
-
-  // console.log("out!!!");
-
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</title></head>");
-  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
-  res.write("</html>");
-  res.end();
-});
+const server = http.createServer(routes.hadler);
 
 server.listen(3000);
