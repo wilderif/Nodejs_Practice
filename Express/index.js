968749827ee9091b80f5715e549df64a0d547c58
wilderif <woojuro3@gmail.com>
2024-07-30 17:34:46 +0900

    feat(Express): Add basic middleware setup
    - Added first middleware that logs a message and passes control to the next middleware.
    - Added second middleware that logs a message and sends an HTML response.

--- Express/index.js
+++ Express/index.js
@@ -4,6 +4,17 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("In the middleware 1");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("In the middleware 2");
+
+  res.send("<h1>Hello from Express</h1>");
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
