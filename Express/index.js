1813bc96686ec4938098d2a65cf3e41733f50032
wilderif <woojuro3@gmail.com>
2024-07-31 12:12:20 +0900

    feat(Express): Add exception handling and route adjustments
    - Added a final middleware for handling access to undefined routes, setting status to 404.
    - Configured the status method to set the response status to 404 for undefined routes.
    - Set '/admin' filter for adminRoutes.
    - Changed '/admin/product' route to '/admin/add-product' to distinguish between GET and POST requests.

--- Express/index.js
+++ Express/index.js
@@ -6,10 +6,13 @@ const app = express();
 const adminRoutes = require("./routes/admin.js");
 const shopRoutes = require("./routes/shop.js");
 
-// body parser
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
 
 app.listen(3000);
