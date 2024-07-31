2c21cfb263096be7e835a1d01293fa3149dcdcc1
wilderif <woojuro3@gmail.com>
2024-07-31 11:44:33 +0900

    feat(Express): Define routes using express.Router
    - Defined ‘/product’ and ‘/add-product’ routes in admin.js using express.Router.
    - Defined ‘/‘ route in shop.js using express.Router.
    - Updated index.js to import and use the routes from admin.js and shop.js.

--- Express/index.js
+++ Express/index.js
@@ -3,27 +3,13 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const adminRoutes = require("./routes/admin.js");
+const shopRoutes = require("./routes/shop.js");
+
 // body parser
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/", (req, res, next) => {
-  // console.log("Always runs!");
-  next();
-});
-
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
-});
-
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from Express</h1>");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
